934b9215c38cf7be3456c7ec8318e04fb65e1ebf
HaniAlshikh <Alshikh.hani@gmail.com>
2019-11-07 19:47:55 +0100

    Aufgabe 02

---
+++ W19/HBN/01.Aufgabe_01/summ.asm
@@ -0,0 +1,42 @@
+; ---------- get input ----------
+
+
+
+01 INP 01
+
+
+; ---------- load input into akku and check it----------
+
+
+
+02 LDA 01
+
+03 JEZ 07
+
+
+
+; ---------- add to akku ----------
+
+
+
+04 ADD 02
+
+05 STA 02
+
+
+
+; ---------- jump for input or print result ----------
+
+
+
+06 JMP 01
+
+07 OUT 02
+
+
+
+; ---------- finish ----------
+
+
+
+08 HLT 99
