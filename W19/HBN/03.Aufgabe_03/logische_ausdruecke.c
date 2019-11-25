c0656a4b1e647f4f639c49d2a694ff1702912735
HaniAlshikh <Alshikh.hani@gmail.com>
2019-11-25 16:05:03 +0100

    1. Logische Ausdrücke

---
+++ W19/HBN/03.Aufgabe_03/logische_ausdruecke.c
@@ -0,0 +1,33 @@
+/* logische_ausdruecke.c
+ * Aufgabe 3 – Grundlagen der Programmierung in C
+ * Autor: Hani Alshikh
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int main () {
+
+   const int cases_num = 4;
+   int i = 34; int j = 23; int k = 1155;
+
+   int tests[cases_num] = {
+      // i, j und k sind alle ungleich 0
+      i ^ 0 && j ^ 0 && k ^ 0,
+      // i ist durch 17 teilbar und echt positiv.
+      i > 0 && i % 17 == 0,
+      // j ist ungerade und liegt zwischen 20 und 40.
+      j > 20 && j < 40 && j % 2 > 0,
+      // k ist Vielfaches von 3 und 5 oder Vielfaches von 5 und 7 oder Vielfaches von 5 und 11.
+      (k % 3 == 0 && k % 5 == 0) || (k % 5 == 0 && k % 5 == 7) || (k % 7 == 0 && k % 5 == 11)
+   };
+
+   for (int i = 0; i < cases_num; i++){
+      printf("%s\n", tests[i] ? "True" : "False");
+   }
+   
+  	return EXIT_SUCCESS;
+}
+
