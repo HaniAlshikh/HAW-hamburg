8d6467236d93562ad40352f3ea36ba326e9579e9
HaniAlshikh <Alshikh.hani@gmail.com>
2019-11-29 13:23:11 +0100

    prof improvments

--- W19/HBN/03.Aufgabe_03/zahlenraten.c
+++ W19/HBN/03.Aufgabe_03/zahlenraten.c
@@ -6,10 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 int main() {      
 
 	// define variables
+  bool found = false;
   const int numbers = 10;
 	srand(time(NULL));
 	int random = rand() % numbers + 1;
@@ -18,17 +20,20 @@ int main() {
 	printf("\nWillkommen! Bitte rate eine Zahl zwischen 1 und 10:\n\n");
 
   // get and check input for random
-  for (int i = 1; i <= numbers; i++) {
+  for (int i = 1; i <= numbers && !found; i++) {
     printf("%d. Versuch: ", i);
     scanf("%2d", &num);
 
     if (num == random) { 
       printf("\nErgebnis: Glückwunsch! Die Zahl wurde im %d. Versuch korrekt geraten!\n", i);
-      break;
+      found = true;
       }
-      
-    if (num > random) { printf("Ergebnis: Die Zahl ist leider zu groß!\n"); }
-    if (num < random) { printf("Ergebnis: Die Zahl ist leider zu klein!\n"); }
+    else if (num > random) {
+       printf("Ergebnis: Die Zahl ist leider zu groß!\n"); 
+      }
+    else {
+       printf("Ergebnis: Die Zahl ist leider zu klein!\n"); 
+      }
   }
 	
 	return EXIT_SUCCESS;
