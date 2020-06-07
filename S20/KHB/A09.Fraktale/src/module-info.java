9a7d5c62da6644fb7cae9e578a8fd105e92f3860
HaniAlshikh <Alshikh.hani@gmail.com>
2020-06-07 17:17:17 +0200

    initial commiit

---
+++ S20/KHB/A09.Fraktale/src/module-info.java
@@ -0,0 +1,8 @@
+module A09.Fraktale {
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+
+    opens de.hawh.kahlbrandt.fx.mandelbrot;
+}
